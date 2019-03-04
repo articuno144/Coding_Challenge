cb83d7876bae5d1e8780b1068e5b01ceb4c036e0
oliver <richtero@ethz.ch>
2019-03-04 18:24:11 +0100

    adds comments

--- coding_challenge/coding_challenge/battleship/battleship.py
+++ coding_challenge/coding_challenge/battleship/battleship.py
@@ -2,7 +2,26 @@ import gym
 from gym import spaces
 import numpy as np
 
-
+"""
+Battleship Environment. This environment implements a simple one player version of the Battleship game where the player
+has to sink all a-priori invisible ships as fast as possible by dropping bombs onto the playing field.
+At the beginning of each episode 10 ships are placed onto the 10x10 grid:
+ - 1 ship of 4 grid cells length (air plane carrier)
+ - 2 ships of 3 gird cells length each (battle cruisers)
+ - 3 ships of 2 grid cells length each (frigates)
+ - 4 ships occupying a single grid cell (submarines)
+Ships are placed such that no ship is directly adjacent or overlapping any other ship.
+To play the game, an agent should create the environment through gym (gym.make('Battleship-v0')), reset it to get the
+initial state and then iteratively call the step function to drop bombs.
+States are given by a tensor of shape (10, 10, 1), i.e., a gray scale image of 10x10 pixels, where a value of 0.0
+encodes no knowledge about the particular field, 0.5 encodes a miss in the given field and 1.0 encodes a hit in the
+given field.
+Actions handed to the environment must be two dimensional (representing x and y coordinate) and lie within the range
+[0,1] to be counted as valid.
+Invalid actions will result in a reward of -1.0
+Hits will result in a reward of 1.0
+Misses will result in a reward of -0.1
+"""
 class BattleshipEnv(gym.Env):
     def __init__(self, max_steps=100):
         self.max_steps = max_steps
@@ -38,7 +57,7 @@ class BattleshipEnv(gym.Env):
         self._ships = np.zeros(shape=(10, 10, 1))
         for air_plane_carrier in range(1):
             self._place_ship(4)
-        for battlecruiser in range(2):
+        for battle_cruiser in range(2):
             self._place_ship(3)
         for frigate in range(3):
             self._place_ship(2)
@@ -57,7 +76,7 @@ class BattleshipEnv(gym.Env):
         terminal = False
         self._step += 1
         if self._step > self.max_steps:
-            info = {'game_message': 'You loose!'}
+            info = {'game_message': 'You lose!'}
             reward = 0.0
             terminal = True
         elif x_idx < 0 or x_idx >= 10 or y_idx < 0 or y_idx >= 10:
--- random_agent/random_agent.py
+++ random_agent/random_agent.py
@@ -2,6 +2,11 @@ import gym
 import coding_challenge
 import numpy as np
 
+"""
+Creates an environment of the Battleship game and plays one episode in it by randomly choosing the actions.
+The actions handed to the Battleship environment should be 2 dimensional (x and y position of the bomb you want to
+drop) and should be scaled to the range [0,1]. The actions are internally discretized to the 10 by 10 playing field.
+"""
 env = gym.make('Battleship-v0')
 state = env.reset()
 terminal = False
